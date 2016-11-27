edb233e8207a30739225c44b06e2f9956bac8c09
locatw <loca@live.jp>
2016-11-27 20:36:33 +0900

    add Logging module tests

--- FavDrop/FavDropTest/LoggingTest.fs
+++ FavDrop/FavDropTest/LoggingTest.fs
@@ -5,27 +5,32 @@ open FsUnit
 open Microsoft.FSharp.Core.LanguagePrimitives
 open Microsoft.WindowsAzure.Storage.Table
 open NUnit.Framework
+open System.Collections.Generic
 open System.Threading.Tasks
 
 [<TestFixture>]
 module LoggerTest =
+    let emptyStorage (getHttpStatusCode : unit -> int) (storedEntities : IList<ITableEntity>) =
+        { new FavDrop.Storage.ITableStorage
+          with
+            member __.InsertAsync(entity) =
+                storedEntities.Add(entity)
+                Task.Factory.StartNew(fun () ->
+                    let result = new TableResult()
+                    result.HttpStatusCode <- getHttpStatusCode()
+                    result) }
+
+    let defaultRetryConfig = 
+        { WaitTime = Int32WithMeasure(1)
+          MaxWaitTime = Int32WithMeasure(1) }
+
     [<Test>]
     let ``store log message into storage once`` () =
-        let mutable storedEntities = []
-        let emptyStorage =
-            { new FavDrop.Storage.ITableStorage
-              with
-                member __.InsertAsync(entity) =
-                    storedEntities <- entity :: storedEntities
-                    Task.Factory.StartNew(fun () ->
-                        let result = new TableResult()
-                        result.HttpStatusCode <- 200
-                        result) }
+        let getHttpStatusCode () = 200
+        let storedEntities = new List<ITableEntity>()
+        let emptyStorage = emptyStorage getHttpStatusCode storedEntities
 
-        let retryConfig =
-            { WaitTime = Int32WithMeasure(1)
-              MaxWaitTime = Int32WithMeasure(1) }
-        use logContext = makeContext emptyStorage retryAsync retryConfig
+        use logContext = makeContext emptyStorage retryAsync defaultRetryConfig
 
         run logContext |> Async.Start
         log logContext Information "test"
@@ -35,7 +40,53 @@ module LoggerTest =
         cancel logContext
 
         storedEntities
+        |> Seq.toList
         |> List.map (fun entity -> (entity :?> LogRecordEntity).Message)
         |> should equal ["test"]
 
+    [<Test>]
+    let ``cancel logging`` () =
+        let getHttpStatusCode () = 200
+        let storedEntities = new List<ITableEntity>()
+        let emptyStorage = emptyStorage getHttpStatusCode storedEntities
 
+        use logContext = makeContext emptyStorage retryAsync defaultRetryConfig
+
+        run logContext |> Async.Start
+        log logContext Information "test"
+
+        Async.Sleep(100) |> Async.RunSynchronously
+
+        cancel logContext
+
+        log logContext Information "test2"
+
+        storedEntities
+        |> Seq.toList
+        |> List.map (fun entity -> (entity :?> LogRecordEntity).Message)
+        |> should equal ["test"]
+
+    [<Test>]
+    let ``insert record failed, then retry inserting`` () =
+        let getHttpStatusCode =
+            let codes = ref [400; 200]
+            (fun () ->
+                let code = List.head !codes
+                codes := List.tail !codes
+                code)
+        let storedEntities = new List<ITableEntity>()
+        let emptyStorage = emptyStorage getHttpStatusCode storedEntities
+
+        use logContext = makeContext emptyStorage retryAsync defaultRetryConfig
+
+        run logContext |> Async.Start
+        log logContext Information "test"
+
+        Async.Sleep(100) |> Async.RunSynchronously
+
+        cancel logContext
+
+        storedEntities
+        |> Seq.toList
+        |> List.map (fun entity -> (entity :?> LogRecordEntity).Message)
+        |> should equal ["test"; "test"]
